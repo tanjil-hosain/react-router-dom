df1769028137097fbd82fb8c7f83562e01a10d24
tanjil-hosain <tanjilh85@gmail.com>
2026-06-07 19:59:30 +0600

    header

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './pages/Home';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   <App></App>
+   <Home />
   </React.StrictMode>
 );
 
