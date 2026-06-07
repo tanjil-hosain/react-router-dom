a747a669358c8ab5c240a3a7137d575104bd1a62
tanjil-hosain <tanjilh85@gmail.com>
2026-06-07 20:05:12 +0600

    Nav And body--Components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
+import Nav from '../components/Nav'
+import Body from '../components/Body'
 
 export default function Home() {
   return (
     <>
+    <Nav />
     <Header/>
+    <Body/>
     </>
   )
 }
